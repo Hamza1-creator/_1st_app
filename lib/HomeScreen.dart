ce047543508b8a0c3bc828a3e24944b17359bd7f
Mujtaba <123797571+Mujtabaa-Ahmed@users.noreply.github.com>
2025-01-01 11:28:36 +0500

    _loading_screen
    added_loadingScreen

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -13,7 +13,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
-
+  late Future<void> _loadingFuture;
 
   final List<Widget> _pages = [
     const Messages(),
@@ -29,59 +29,98 @@ class _HomeScreenState extends State<HomeScreen> {
     "Calling",
   ];
 
+ 
+  Future<void> _initializeApp() async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadingFuture = _initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea( 
-      child : Scaffold(
-      backgroundColor: const Color.fromARGB(255, 77, 83, 88),
-     appBar: AppBar(
-      backgroundColor: const Color.fromARGB(255, 77, 83, 88),
-      toolbarHeight: 40.0,
-        title: Text(_titles[_currentIndex] ,style: TextStyle(color: const Color.fromARGB(255, 236, 236, 236)),),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            color: const Color.fromARGB(255, 236, 236, 236),
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text("Search tapped")),
-              );
-            },
-          ),
-        ],
-      ),
-
-      body: _pages[_currentIndex], 
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color.fromARGB(255, 77, 83, 88),
-        selectedItemColor: const Color.fromARGB(255, 236, 236, 236),
-        unselectedItemColor: const Color.fromARGB(255, 201, 202, 202),
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_sharp),
-            label: "Messages",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: "Cart",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: "Calling",
-          ),
-        ],
-      ),
-    ),);
+    return FutureBuilder(
+      future: _loadingFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            backgroundColor: const Color.fromARGB(255, 77, 83, 88),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 150, 
+                    child: LinearProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                      minHeight: 3,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return SafeArea(
+            child: Scaffold(
+              backgroundColor: const Color.fromARGB(255, 77, 83, 88),
+              appBar: AppBar(
+                backgroundColor: const Color.fromARGB(255, 77, 83, 88),
+                toolbarHeight: 40.0,
+                title: Text(
+                  _titles[_currentIndex],
+                  style: const TextStyle(color: Color.fromARGB(255, 236, 236, 236)),
+                ),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    color: const Color.fromARGB(255, 236, 236, 236),
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("Search tapped")),
+                      );
+                    },
+                  ),
+                ],
+              ),
+              body: _pages[_currentIndex],
+              bottomNavigationBar: BottomNavigationBar(
+                backgroundColor: const Color.fromARGB(255, 77, 83, 88),
+                selectedItemColor: const Color.fromARGB(255, 236, 236, 236),
+                unselectedItemColor: const Color.fromARGB(255, 201, 202, 202),
+                type: BottomNavigationBarType.fixed,
+                currentIndex: _currentIndex,
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.chat_sharp),
+                    label: "Messages",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.shopping_bag),
+                    label: "Cart",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    label: "Settings",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.call),
+                    label: "Calling",
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
+    );
   }
 }
